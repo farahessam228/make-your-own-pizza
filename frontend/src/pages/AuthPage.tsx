a00f447b9b541368c22fd3510df9156fd1388613
Kkarim-Ahmed <kkarim.1.aahmed@gmail.com>
2026-08-13 01:20:46 +0300

    feat: neseet ahot dol kaman

--- frontend/src/pages/AuthPage.tsx
+++ frontend/src/pages/AuthPage.tsx
@@ -1,38 +1,36 @@
-import React,{useState}from "react";
-import LoginForm from "../components/auth/LoginForm";
-import SignUpForm from "../components/auth/SignUpForm";
-import './AuthPage.css'
-export default function AuthPage(){
+import React, { useState } from "react";
+import LoginForm from "@/components/auth/LoginForm";
+import SignUpForm from "@/components/auth/SignUpForm";
+import "./AuthPage.css"
+export default function AuthPage() {
     const [isLogin, setIsLogin] = useState(true);
 
     return (
-    <div className="auth-container ">
-        <div className="pizza-ring ring-1"></div>
-        <div className="pizza-ring ring-2"></div>
-        <div className="pizza-ring ring-3"></div>
-        <div className="pizza-ring ring-4"></div>
-        <div className="pizza-ring ring-5"></div>
-        <div className="pizza-ring ring-6"></div>
+        <div className="auth-container ">
+            <div className="pizza-ring pizza-ring--1"></div>
+            <div className="pizza-ring pizza-ring--2"></div>
+            <div className="pizza-ring pizza-ring--3"></div>
+            <div className="pizza-ring pizza-ring--4"></div>
 
-        <div className="topping t-1"></div>
-        <div className="topping t-2"></div>
-        <div className="topping t-3"></div>
-        <div className="topping t-4"></div>
+            <div className="topping t-1"></div>
+            <div className="topping t-2"></div>
+            <div className="topping t-3"></div>
+            <div className="topping t-4"></div>
 
-        <div className={`form-card ${isLogin ? 'login-mode' : 'signup-mode'}`}>
-            {isLogin ? <LoginForm /> : <SignUpForm />}
-            <div className="submit">
-                <p>
-                    {isLogin ? "Don't have an account? " : "Already have an account? "}
-                    <button 
-                        onClick={() => setIsLogin(!isLogin)}
-                        className="toggle-btn"
+            <div className={`form-card ${isLogin ? 'login-mode' : 'signup-mode'}`}>
+                {isLogin ? <LoginForm /> : <SignUpForm />}
+                <div className="submit">
+                    <p>
+                        {isLogin ? "Don't have an account? " : "Already have an account? "}
+                        <button
+                            onClick={() => setIsLogin(!isLogin)}
+                            className="toggle-btn"
                         >
-                        {isLogin ? 'Sign Up' : 'Login'}
-                    </button>
-                </p>
+                            {isLogin ? 'Sign Up' : 'Login'}
+                        </button>
+                    </p>
+                </div>
             </div>
         </div>
-    </div>
     );
 }
